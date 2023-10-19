c40f5b71896c3ee7e284df33ef27fedb909b3608
Table 4 <touloniut@gmail.com>
2023-10-19 11:53:20 +0200

    TP1 début

---
+++ Embedded/Robot_Djebara_Nguewo.X/mainc.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   mainc.c
+ * Author: TP_EO_6
+ *
+ * Created on 19 octobre 2023, 10:53
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return (EXIT_SUCCESS);
+}
+
